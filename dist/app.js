5b2daa31bd9532adedc3349486e3ba59fe95c84f
houzairkoussa <houzairmk@icloud.com>
2022-01-19 12:23:55 +0400

    ADD user input handler, autobin decorator for submit  method

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,47 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autoBind(_, __, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        get() {
+            const boundFunction = originalMethod.bind(this);
+            return boundFunction;
+        }
+    };
+    return adjustedDescriptor;
+}
+;
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInput = this.element.querySelector('#title');
+        this.descriptionInput = this.element.querySelector('#description');
+        this.peopleInput = this.element.querySelector('#people');
+        this.config();
         this.attach();
+    }
+    submit(e) {
+        e.preventDefault();
+    }
+    config() {
+        this.element.addEventListener('submit', this.submit);
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
+__decorate([
+    autoBind
+], ProjectInput.prototype, "submit", null);
 const projectInput_ = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
+// decorators
+function autoBind (_: any, __: string | symbol, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+      return boundFunction;
+    }
+  };
+  return adjustedDescriptor;
+};
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInput: HTMLInputElement;
+  descriptionInput: HTMLInputElement;
+  peopleInput: HTMLInputElement;
 
   constructor () {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,7 +25,24 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    this.element.id = 'user-input';
+    this.titleInput = this.element.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInput = this.element.querySelector('#description')! as HTMLInputElement;
+    this.peopleInput = this.element.querySelector('#people')! as HTMLInputElement;
+
+    this.config();
     this.attach();
+  }
+
+  @autoBind
+  private submit (e: Event) {
+    e.preventDefault();
+    // todo
+  }
+
+  private config () {
+    this.element.addEventListener('submit', this.submit);
   }
 
   private attach () {
@@ -17,5 +50,5 @@ class ProjectInput {
   }
 }
 
-// START PROJECT
+// INIT FORM
 const projectInput_: ProjectInput = new ProjectInput();
